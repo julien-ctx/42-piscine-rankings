541160b9d688015aa7348b6b540de6f00e249442
julien-ctx <jdaodkql@gmail.com>
2023-01-30 15:36:07 +0100

    Piscine logtime ok

--- config.py
+++ config.py
@@ -1,2 +1,14 @@
 campus_id = 41
 cursus_id = 1
+
+class color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96;1m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94;1m'
+   GREEN = '\033[92;1m'
+   YELLOW = '\033[93;1m'
+   RED = '\033[91;1m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   RESET = '\033[0m'
--- piscine_logtime.py
+++ piscine_logtime.py
@@ -1,14 +1,15 @@
 from intra import ic
 from config import campus_id
-import sys, webbrowser
+from config import color as color
+import sys
 import pandas as pd
 from datetime import datetime, timedelta
 
-def get_logtime(id):
+def get_logtime(id, start_time, end_time):
     payload = {
         "filter[campus_id]": campus_id,
-        'begin_at': '2021-09-05',
-        'end_at': '2021-10-02'
+        'begin_at': start_time,
+        'end_at': end_time
     }
 
     response = ic.get(f"users/{id}/locations_stats", params=payload)
@@ -24,24 +25,54 @@ def get_logtime(id):
         return 0, 0
     return hours, minutes
 
+def get_users():
+    # Get all users of a piscine
+    users = []
+    page_num = 1
+    pool_month = input(color.BLUE + "POOL MONTH (letters): " + color.RESET)
+    pool_year = input(color.BLUE + "POOL YEAR (numbers): " + color.RESET)
+    start_time = input(color.BLUE + "START DATE (YYYY-MM-DD): " + color.RESET)
+    end_time = input(color.BLUE + "END DATE (YYYY-MM-DD): " + color.RESET)
+
+    while (True):
+        payload = {
+            'filter[pool_month]': pool_month,
+            'filter[pool_year]': pool_year,
+            'campus_id': campus_id,
+            'page[size]': 100,
+            'page[number]': page_num,
+        }
+        response = ic.get(f"campus/{campus_id}/users", params=payload)
+        if response.status_code == 200:
+            data = response.json()
+            if not data:
+                break
+        else:
+            sys.exit(f"Unexpected response: code ${response.status_code}")   
+        for user in data:
+            users.append(user['id'])
+        page_num += 1
+    return users, start_time, end_time
+
 if __name__ == "__main__":
     page_num = 1
-    chrome = webbrowser.get('chrome')
     df = pd.DataFrame({
         'id': [],
         'name': [],
         'level': [],
         'login': [],
         'logtime_hours': [],
-        'logtime_min': []
+        'logtime_min': [],
+        'total_min': []
     })
+
+    users, start_time, end_time = get_users()
 
     while True:
         payload = {
             "filter[campus_id]": campus_id,
             "page[size]": 100,
             "page[number]": page_num,
-            "sort[level]": "desc",
             "filter[cursus_id]": 9
         }
 
@@ -51,22 +82,22 @@ if __name__ == "__main__":
             if not data:
                 break
             for user in data:
-                if (user['user']['pool_month'] == "september"
-                    and user['user']['pool_year'] == "2021"):
-                    hours, minutes = get_logtime(user['user']['id'])
+                if (user['user']['id'] in users):
+                    hours, minutes = get_logtime(user['user']['id'], start_time, end_time)
                     new_row = pd.DataFrame({
                         'id': [int(user['user']['id'])],
                         'name': [user['user']['displayname']],
                         'level': [float(user['level'])],
                         'login': [user['user']['login']],
                         'logtime_hours': [int(hours)],
-                        'logtime_min': [int(minutes)]
+                        'logtime_min': [int(minutes)],
+                        'total_min': [int(hours) * 24 + int(minutes)]
                     })
                     df = pd.concat([df, new_row], ignore_index=True)
             page_num += 1
         else:
             sys.exit(f"Unexpected response: code ${response.status_code}")
 
-    df = df.sort_values(by='logtime_hours')
-    df.to_csv('logtime_september_2021.csv', index=False)
+    df = df.sort_values(by='total_min', ascending=False)
+    df.to_csv('february.csv', index=False)
 
